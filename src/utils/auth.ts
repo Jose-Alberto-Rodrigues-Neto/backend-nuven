8dd5cf7f6c970fb47ff7393a42237af910dbb143
Alberto Rodrigues <allio.professional@gmail.com>
2025-07-20 09:47:55 -0300

    feat: creating auth functions

---
+++ src/utils/auth.ts
@@ -0,0 +1,51 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import { NextFunction, Request, Response } from 'express';
+
+dotenv.config()
+
+const secret = process.env.JWT_SECRET
+
+type TokenPayload = {
+    userId: string,
+    iat: number;
+    exp: number
+}
+
+interface RequestUser extends Request{
+    user?:{
+        id:string
+    }
+}
+
+export function generateToken(payload: object): string {
+  return jwt.sign(payload, secret, { expiresIn: '5m' });
+}
+
+export function verifyToken(token){
+    return jwt.verify(token, secret)
+}
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).json({
+            error: "É preciso do token de acesso!"
+        })
+        return
+    }
+
+    const [, token] = authHeader.split(' ')
+
+    try{
+        const decoded = jwt.verify(token, secret) as TokenPayload
+        const reqUser = req as RequestUser
+        reqUser.user = {
+            id: decoded.userId
+        }
+        next()
+    }catch(error){
+        res.status(401).json({ error: "Token inválido ou expirado!" })
+        return
+    }
+}
