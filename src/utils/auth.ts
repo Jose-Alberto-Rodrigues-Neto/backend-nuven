744eb145de0096d3e8a90f59c09da1a77fc7b320
Alberto Rodrigues <allio.professional@gmail.com>
2025-07-20 10:49:39 -0300

    fix: fixing authMiddleware

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -19,33 +19,29 @@ interface RequestUser extends Request{
 }
 
 export function generateToken(payload: object): string {
-  return jwt.sign(payload, secret, { expiresIn: '5m' });
+    return jwt.sign(payload, secret, { expiresIn: '30m' });
 }
 
 export function verifyToken(token){
     return jwt.verify(token, secret)
 }
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction){
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        res.status(401).json({
-            error: "É preciso do token de acesso!"
-        })
-        return
-    }
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
 
-    const [, token] = authHeader.split(' ')
+  if (!authHeader) {
+    res.status(401).json({ error: "É preciso do token de acesso!" });
+    return;
+  }
 
-    try{
-        const decoded = jwt.verify(token, secret) as TokenPayload
-        const reqUser = req as RequestUser
-        reqUser.user = {
-            id: decoded.userId
-        }
-        next()
-    }catch(error){
-        res.status(401).json({ error: "Token inválido ou expirado!" })
-        return
-    }
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = jwt.verify(token, secret) as TokenPayload;
+    const reqUser = req as RequestUser;
+    reqUser.user = { id: decoded.userId };
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token inválido ou expirado!" });
+  }
 }
