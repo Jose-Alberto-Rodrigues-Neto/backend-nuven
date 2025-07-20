02d5a8c7403585b6875ab1fb668d30b287958f9e
Alberto Rodrigues <allio.professional@gmail.com>
2025-07-20 08:58:22 -0300

    feat: adding swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,41 @@
 import express from 'express';
+import userRoutes from './routes/UserRoutes.js';
+import dotenv from 'dotenv';
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
-const port = 8080
+dotenv.config
+const port = process.env.PORT || 8080
+const swaggerDefinition = {
+    openapi: '3.0.0',
+  info: {
+    title: 'Swagger',
+    version: '1.0.0',
+    description: 'Documentação das rotas da API',
+  },
+  servers: [
+    {
+      url: 'http://localhost:8080',
+    },
+  ],
+}
+const options = {
+    swaggerDefinition,
+    apis: ['./src/routes/*.ts']
+}
+const swaggerJsDoc = swaggerJSDoc(options)
 
-//midleware
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsDoc));
+
 
 app.get('/', (req, res) => {
     res.status(200).send("Ola mundo")
 })
+
 app.listen(port, () => 
     console.log(`Example app listening on port ${port}!\n`)
 )
+
+app.use("/users", userRoutes)
